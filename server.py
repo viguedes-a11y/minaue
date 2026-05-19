5de81fd944bdf0d107bf5a82ffcf6ea3bc401dac
Viviane Guedes <vi.guedes@icloud.com>
2026-05-19 16:59:04 -0300

    Central de Postagens Minaue — deploy inicial

---
+++ server.py
@@ -0,0 +1,9 @@
+import os
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return FileResponse("central-postagens.html")
